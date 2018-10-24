5de2d04452117bd0bd778fcfaae56b74fd2795e4
MartinK-NTB <martin.kotena84@gmail.com>
2018-10-24 15:51:12 +0200

    HistoryProvider - method save to Storage Added

--- src/models/history-record.ts
+++ src/models/history-record.ts
@@ -2,7 +2,7 @@ export class HistoryRecord{
     public from:string;
     public to:string;
 
-    cnstructor(from:string, to:string){
+    constructor(from:string, to:string){
         this.from = from;
         this.to = to;
     }
